828bee2906d8fee1451794bb8a89cb42fac8e991
Jesse Tane <jesse.tane@gmail.com>
2014-10-12 15:54:03 -0400

    don't hardcode extensions

--- index.js
+++ index.js
@@ -2,22 +2,14 @@ var thru = require('through2').obj;
 var quote = require('quote-stream');
 var combine = require('stream-combiner2');
 
-var extensions = [
-  'txt',
-  'text',
-  'html',
-  'css',
-  'csv',
-  'tmpl',
-  'tpl'
-];
-
-var regex = new RegExp('\.(' + extensions.join('|') + ')$');
-
 module.exports = txtify;
 
-function txtify(file) {
-  if (regex.test(file)) {
+function txtify(file, opts) {
+  var extensions = [];
+  var optional = opts.extensions || opts.e;
+  for (var i in optional) extensions.push(optional[i]);
+  var matcher = new RegExp('\.(' + extensions.join('|') + ')$');
+  if (matcher.test(file)) {
     var first = true;
     return combine(
       quote(),
